b589e6166a9821f0dc5c0c2442575ce580317940
Ksenia <kseniarlog@gmail.com>
2025-02-16 14:33:24 +0300

    feat(Core): add Drag&Drop

--- src/App.vue
+++ src/App.vue
@@ -35,36 +35,52 @@
         item-value="id"
         bg-color="transparent"
       >
-        <v-list-item
-          v-for="(item, index) in sortedTodos"
+        <div
+          v-for="(item, index) in todos"
           :key="item.id"
-          :value="item"
+          @drop="onDrop($event, item.id)"
+          @dragenter.prevent
+          @dragover.prevent
         >
-          <template #prepend>
-           <div class="d-flex ga-3 align-center">
-             #{{ index + 1 }}
+          <v-list-item
+            :ref="(el) => dragImages[item.id] = el"
+            :value="item"
+          >
+            <template #prepend>
+             <div class="d-flex ga-3 align-center">
+               #{{ index + 1 }}
 
-             <v-list-item-action class="pt-6" start>
-               <v-checkbox
-                 v-model="item.isDone"
-                 color="info"
-               />
-             </v-list-item-action>
-           </div>
-          </template>
+               <v-list-item-action class="pt-6" start>
+                 <v-checkbox
+                   v-model="item.isDone"
+                   color="info"
+                 />
+               </v-list-item-action>
+             </div>
+            </template>
 
-          {{ item.title }}
+            {{ item.title }}
 
-          <template #append>
-            <v-btn
-              block
-              color="red-accent-4"
-              @click="onDeleteButtonClick(item.id)"
-            >
-              delete
-            </v-btn>
-          </template>
-        </v-list-item>
+            <template #append>
+              <v-btn
+                icon="mdi-delete"
+                color="red-accent-4"
+                title="delete"
+                aria-label="delete todo item"
+                @click="onDeleteButtonClick(item.id)"
+              />
+              <v-btn
+                icon="mdi-drag"
+                variant="plain"
+                size="x-large"
+                title="transfer"
+                aria-label="transfer todo item"
+                draggable="true"
+                @dragstart="onDragStart($event, item.id)"
+              />
+            </template>
+          </v-list-item>
+        </div>
       </v-list>
 
       <div class="text-center" v-else>Your haven't any deals yet</div>
@@ -74,15 +90,12 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted } from 'vue'
 const isLoading = ref(false)
 const counter = ref(1)
 const inputModelValue = ref('')
 const todos = ref([])
-
-const sortedTodos = computed(() => {
-  return todos.value.sort((a, b) => b.isDone - a.isDone)
-})
+const dragImages = ref([])
 
 const loadPage = () => {
   isLoading.value = true
@@ -90,7 +103,7 @@ const loadPage = () => {
 }
 
 const onAddDealButtonClick = () => {
-  todos.value.push({ title: inputModelValue.value, id: counter.value, isDone: false })
+  todos.value.push({ title: inputModelValue.value, id: String(counter.value), isDone: false })
   inputModelValue.value = ''
   counter.value += 1
 }
@@ -100,5 +113,36 @@ const onDeleteButtonClick = (id) => {
   todos.value.splice(foundedTodoIndex, 1)
 }
 
+const onDragStart = (evt, id) => {
+  const dragImage = dragImages.value[id].$el
+  const buttonHeight = evt.target.clientHeight
+  const buttonWidth = evt.target.clientWidth
+  const topGapToButton = (dragImage.clientHeight - buttonHeight) / 2
+  const rightGapToButton = 16
+
+  evt.dataTransfer.dropEffect = 'move'
+  evt.dataTransfer.effectAllowed = 'move'
+  evt.dataTransfer.setDragImage(dragImage, dragImage.clientWidth - (buttonWidth - evt.offsetX) - rightGapToButton, topGapToButton + evt.offsetY);
+  evt.dataTransfer.setData('itemId', id)
+}
+
+const onDrop = (evt, droppedId) => {
+  const transferredId = evt.dataTransfer.getData('itemId')
+  const transferredTodoItem = todos.value.find(({ id }) => id === transferredId)
+
+  todos.value = todos.value.reduce((acc, item, index) => {
+    const isLast = todos.value.length === index + 1
+    const currentTodoId = item.id
+
+    // по дефолту вставляем перетаскиваемый элемент выше дропнутого.
+    // Проблема: Получаем ситуацию, что невозможно перенести перетаскиваемый элемент на последнюю позицию.
+    // Решение: Флаг isLast позволяет перенести транспортированный элемент на последнюю позицию в списке.
+    if (!isLast && currentTodoId === droppedId) acc.push(transferredTodoItem)
+    if (currentTodoId !== transferredId) acc.push(item)
+    if (isLast && currentTodoId === droppedId) acc.push(transferredTodoItem)
+
+    return acc
+  }, [])
+}
 onMounted(loadPage)
 </script>
